7a53cecae83b521363ebaa1c90bddcdbba597c4d
Jens Rosseau <jens.rosseau@gmail.com>
2018-09-27 10:06:26 +0200

    Read json into main activity

--- app/src/main/java/be/ap/edu/veloapplication/MainActivity.java
+++ app/src/main/java/be/ap/edu/veloapplication/MainActivity.java
@@ -1,13 +1,66 @@
 package be.ap.edu.veloapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends Activity {
 
+    public static String getAssetJsonData(Context context) {
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open("velostation.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        Log.e("data", json);
+        return json;
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            JSONObject obj = new JSONObject(getAssetJsonData());
+            JSONArray m_jArry = obj.getJSONArray("velostations");
+            ArrayList<HashMap<String, String>> formList = new ArrayList<HashMap<String, String>>();
+            HashMap<String, String> m_li;
+
+            for (int i = 0; i < m_jArry.length(); i++) {
+                JSONObject jo_inside = m_jArry.getJSONObject(i);
+                Log.d("Details-->", jo_inside.getString("naam"));
+                String naam_value = jo_inside.getString("naam");
+                String lat_value = jo_inside.getString("point_lat");
+                String long_value = jo_inside.getString("point_lng");
+
+                //Add your values in your `ArrayList` as below:
+                m_li = new HashMap<String, String>();
+                m_li.put("naam", naam_value);
+                m_li.put("lat_value", lat_value);
+
+                formList.add(m_li);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
